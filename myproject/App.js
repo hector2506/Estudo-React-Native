84a3d6cfadf72c025092e948751c7909ab17dc7a
hector batista <hectorbatista500@hotmail.com>
2020-10-06 14:25:59 -0300

    visto Flatlist

--- myproject/App.js
+++ myproject/App.js
@@ -1,27 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: 'shaun', key: 1 },
-    { name: 'yoshi', key: 2 },
-    { name: 'mario', key: 3 },
-    { name: 'luigi', key: 4 },
-    { name: 'peach', key: 5 },
-    { name: 'toad', key: 6 },
-    { name: 'bowser', key: 7 },
+    { name: 'shaun', id: 1 },
+    { name: 'yoshi', id: 2 },
+    { name: 'mario', id: 3 },
+    { name: 'luigi', id: 4 },
+    { name: 'peach', id: 5 },
+    { name: 'toad', id: 6 },
+    { name: 'bowser', id: 7 },
   ])
 
   return (
     <View style={styles.container}>
+      <FlatList
+        data={people}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+      {/*   
       <ScrollView>
         {people.map(item => (
           <View key={item.key}>
             <Text style={styles.item}>{item.name}</Text>
           </View>
         ))}
-      </ScrollView>
+        </ScrollView> */}
     </View>
   );
 }
@@ -40,5 +49,7 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: 'pink',
     fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
   },
 });
