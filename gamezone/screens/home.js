3feb4a65f3b664326bc79e45d87205fc42bb4362
hector batista <hectorbatista500@hotmail.com>
2020-10-12 07:26:19 -0300

    adicionadas formik forms

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { View, Text, FlatList, TouchableOpacity, Modal, StyleSheet } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Modal, StyleSheet } from 'react-native'
 import Card from '../shared/card'
 import { globalStyles } from '../styles/global'
 import { MaterialIcons } from '@expo/vector-icons'
+import ReviewForm from './reviewForm'
 
 export default function Home({ navigation }) {
     const [modalOpen, setModalOpen] = useState(false)
@@ -12,20 +13,29 @@ export default function Home({ navigation }) {
         { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
         { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
     ])
+    const addReview = (review) => {
+        review.key = Math.random().toString()
+        setReviews((currentReviews) => {
+            return [review, ...currentReviews]
+        })
+        setModalOpen(false)
+    }
 
     return (
         <View style={globalStyles.container}>
 
             <Modal visible={modalOpen} animationType='slide'>
-                <View style={styles.modalContent}>
-                    <MaterialIcons
-                        name='close'
-                        size={24}
-                        style={{ ...styles.modalToggle, ...styles.modalClose }}
-                        onPress={() => setModalOpen(false)}
-                    />
-                    <Text>Hello from the modal.</Text>
-                </View>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.modalContent}>
+                        <MaterialIcons
+                            name='close'
+                            size={24}
+                            style={{ ...styles.modalToggle, ...styles.modalClose }}
+                            onPress={() => setModalOpen(false)}
+                        />
+                        <ReviewForm addReview={addReview} />
+                    </View>
+                </TouchableWithoutFeedback>
             </Modal>
 
             <MaterialIcons
