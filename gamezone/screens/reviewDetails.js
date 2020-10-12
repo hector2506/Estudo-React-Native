b5c444267c0eb8597a98b0e2001750f82360463e
hector batista <hectorbatista500@hotmail.com>
2020-10-11 22:50:19 -0300

    adicionadas imagens a reviewDetails

--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import { globalStyles } from '../styles/global'
+import { StyleSheet, View, Text, Image } from 'react-native'
+import Card from '../shared/card'
+import { globalStyles, images } from '../styles/global'
 
 export default function ReviewDetails({ navigation }) {
+    const rating = navigation.getParam('rating')
 
     return (
         <View style={globalStyles.container}>
-            <Text>{navigation.getParam('title')}</Text>
-            <Text>{navigation.getParam('body')}</Text>
-            <Text>{navigation.getParam('rating')}</Text>
+            <Card>
+                <Text>{navigation.getParam('title')}</Text>
+                <Text>{navigation.getParam('body')}</Text>
+                <View style={styles.rating}>
+                    <Text>Gamezone rating: </Text>
+                    <Image source={images.ratings[rating]} />
+                </View>
+            </Card>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: "center",
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    }
+})
