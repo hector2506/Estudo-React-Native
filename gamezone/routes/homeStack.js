0b3b40bd29ca6d812220211ca91dc58ceee0b6d8
hector batista <hectorbatista500@hotmail.com>
2020-10-11 21:15:08 -0300

    melhorada a Navegacao e adicionados nagivation options

--- gamezone/routes/homeStack.js
+++ gamezone/routes/homeStack.js
@@ -5,13 +5,24 @@ import ReviewDetails from '../screens/reviewDetails'
 
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: 'GameZone',
+        }
     },
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: 'Review Details',
+        }
     },
 }
 
-const HomeStack = createStackNavigator(screens)
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerStyle: { backgroundColor: '#eee' },
+        headerTintColor: '#444',
+    }
+})
 
 export default createAppContainer(HomeStack)
